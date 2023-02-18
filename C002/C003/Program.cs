3df7d9b140f13fa61f16b178fdf029fe81706633
NATA <armida780@mail.ru>
2023-02-18 18:35:22 +0300

    if_Else

---
+++ C002/C003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите имя пользователя: ");
+string UserName = Console.ReadLine();
+
+if (UserName.ToLower() == "maria")
+{
+    Console.WriteLine ("Ура, это же Маша");
+}
+else
+{
+    Console.Write("Hello! ");
+    Console.WriteLine(UserName);
+}
